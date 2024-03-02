59b14236755c43e983b704105b149d94ce9b644d
wstypr <wsetyapermadi@gmail.com>
2024-03-02 10:08:33 +0700

    add form and validation

---
+++ script.js
@@ -0,0 +1,8 @@
+const form = document.querySelector("form");
+form.addEventListener("submit", (event) => {
+  if (!form.checkValidity()) {
+    event.preventDefault();
+    event.stopPropagation();
+    form.classList.add("was-validated");
+  }
+});
