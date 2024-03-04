bd8438497cb39233e12433d542e805cca449ac28
wstypr <wsetyapermadi@gmail.com>
2024-03-04 23:30:56 +0700

    connect to appscript

--- script.js
+++ script.js
@@ -1,8 +1,30 @@
 const form = document.querySelector("form");
+
 form.addEventListener("submit", (event) => {
   if (!form.checkValidity()) {
     event.preventDefault();
     event.stopPropagation();
     form.classList.add("was-validated");
+  } else {
+    event.preventDefault();
+    const formData = new FormData(form);
+    fetch(
+      "https://script.google.com/macros/s/AKfycby_wiJKqdANOZLn09EI3gpuJh6gvfsra43pw25CijEzlvuIGvIe_pk0NZkjSuF6pzU/exec",
+      {
+        method: "POST",
+        body: formData,
+      }
+    )
+      .then((res) => {
+        console.log(res);
+        if (res == "success") {
+          console.log("form submitted successfully");
+        } else {
+          console.log("appscript error");
+        }
+      })
+      .catch((error) => {
+        console.error("network error: ", error);
+      });
   }
 });
